4c9a4b9628571f1b232cd002babcbbf18678cf87
riderjensen@gmail.com <riderjensen@gmail.com>
2019-10-21 10:06:58 -0600

    change thank you page

--- assets/js/countUpEx.js
+++ assets/js/countUpEx.js
@@ -4,14 +4,16 @@ window.onload = function () {
     const CountUpOptions = {
         duration: 2, 
         }
-    const countUpOne = new CountUp('countUp1', 900, CountUpOptions);
-    const countUpTwo = new CountUp('countUp2', 500, CountUpOptions);
-    const countUpThree = new CountUp('countUp3', 100000, CountUpOptions);
+    const countUpOne = new CountUp('countUp1', 575, CountUpOptions);
+    const countUpTwo = new CountUp('countUp2', 900, CountUpOptions);
+    const countUpThree = new CountUp('countUp3', 96, CountUpOptions);
+    const countUpFour = new CountUp('countUp3', 100000, CountUpOptions);
+
 
     countUpOne.start();
     countUpTwo.start();
     countUpThree.start();
-
+    countUpFour.start();
   }
   
 
